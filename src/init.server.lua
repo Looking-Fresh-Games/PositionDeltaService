85feaadd2ac94652b1d7e100295e550b24e36217
DevIrradiant <166556662+DevIrradiant@users.noreply.github.com>
2024-11-25 14:12:24 -0600

    fix: restructure how CPD functions

--- src/init.server.lua
+++ src/init.server.lua
@@ -1,8 +1,8 @@
 --!strict
 
 --[[
-    CheckPositionDeltas.lua
-    Author: MichaelSouthern
+    CheckPositionDeltas.server.lua
+    Author: Michael Southern [DevIrradiant]
 
     Description: Monitors the delta of character Positions.
     
@@ -15,7 +15,6 @@
 
     RefreshRate: Seconds per check
     MaxMagnitude: Maximum stud allowance
-    IgnoreState: OPTIONAL - string to 
     DebugMode: Disables kicking, enables warns (great for
     helping configure RefreshRate and MaxMagnitude)
 ]]
@@ -25,7 +24,6 @@ local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 
 -- Types
-
 type Configuration = {
     RefreshRate: number,
     MaxMagnitude: number,
@@ -33,80 +31,94 @@ type Configuration = {
 }
 
 -- Refs
-local Configuration = {
-    RefreshRate = 1,
-    MaxMagnitude = 10,
+local _configuration = {
+    RefreshRate = 0.1,
+    MaxMagnitude = 30,
     DebugMode = true
 } :: Configuration
 
+local _positionData = {} :: {[Player]: Vector3}
+local _respawnConnections = {} :: {[Player]: RBXScriptConnection}
+local _diedConnections = {} :: {[Player]: RBXScriptConnection}
+local _elapsed = 0
+
+
 
 -- Class
-local CheckPositionDeltas = {}
-CheckPositionDeltas.__index = CheckPositionDeltas
-
-function CheckPositionDeltas.new()
-    local self = setmetatable({}, CheckPositionDeltas)
-
-    -- Internal Refs
-    self._positionData = {} :: any
-
-    -- Listen for entering / exiting Players
-    Players.PlayerAdded:Connect(function(player: Player)
-        self:_playerAdded(player)
-    end)
-    Players.PlayerRemoving:Connect(function(player: Player)
-        self:_playerRemoving(player)
-    end)
-
-    -- Setup
-    for _, player: Player in Players:GetPlayers() do
-        self:_playerAdded(player)
-    end
-    self:_init()
-
-    return self
-end
+local PositionDeltaService = {}
 
 -- Create player entry
-function CheckPositionDeltas:_playerAdded(player: Player?)
+function PositionDeltaService:_playerAdded(player: Player?)
     if not player then
         return
     end
 
-    local playerEntry = self._positionData[player]
-    if playerEntry then
-        return
+    -- add new position entry
+    local playerEntry = _positionData[player]
+    if not playerEntry then
+        _positionData[player] = Vector3.zero
     end
 
-    self._positionData[player] = Vector3.zero
+    -- add a respawn connection entry -- prevent false positive when respawning
+    local respawnEntry = _respawnConnections[player]
+    if not respawnEntry then
+        -- listen for new character
+        _respawnConnections[player] = player.CharacterAdded:Connect(function(newCharacter: Model)
+            -- zero out position entry to prevent false positive
+            _positionData[player] = Vector3.zero
+    
+            -- listen for humanoid
+            local humanoid = newCharacter:WaitForChild("Humanoid", 5) :: Humanoid?
+            if humanoid then
+                -- listen for Died, zero out position entry to prevent false positive
+                _diedConnections[player] = humanoid.Died:Once(function()
+                    _positionData[player] = Vector3.zero
+                    _diedConnections[player] = nil
+                end)
+            end
+        end) 
+    end
 end
 
 -- Remove player entry
-function CheckPositionDeltas:_playerRemoving(player: Player?)
+function PositionDeltaService:_playerRemoving(player: Player?)
     if not player then
         return
     end
 
-    local playerEntry = self._positionData[player]
-    if not playerEntry then
-        return
+    -- remove position entry
+    local playerEntry = _positionData[player]
+    if playerEntry then
+        _positionData[player] = nil
     end
 
-    self._positionData[player] = nil
+    -- remove respawn detection entry
+    local respawnEntry = _respawnConnections[player]
+    if respawnEntry then
+        _respawnConnections[player]:Disconnect()
+        _respawnConnections[player] = nil
+    end
+
+    -- remove died connection entry
+    local diedEntry = _diedConnections[player]
+    if diedEntry then
+        _diedConnections[player]:Disconnect()
+        _diedConnections[player] = nil
+    end
 end
 
 -- Player exceeded MaxMagnitude
-function CheckPositionDeltas:_exceededMaxMagnitude(player: Player?)
+function PositionDeltaService:_exceededMaxMagnitude(player: Player?)
     if not player then
         return
     end
 
-    local playerEntry = self._positionData[player]
+    local playerEntry = _positionData[player]
     if not playerEntry then
         return
     end
 
-    if Configuration.DebugMode then
+    if _configuration.DebugMode then
         warn(`{player.Name} has exceeded MaxMagnitude`)
     else
         player:Kick()
@@ -114,46 +126,69 @@ function CheckPositionDeltas:_exceededMaxMagnitude(player: Player?)
 end
 
 -- Compare positions for each player
-function CheckPositionDeltas:_scan()
+function PositionDeltaService:_scan()
     -- filter through all player entries
-    for player: Player, lastPosition: Vector3 in self._positionData do
-        -- TODO verify player isn't being teleported by the server
-        
-        -- verify a character exists
-        local character = player.Character :: Model?
-        if character then
-            local currentPosition = character:GetPivot().Position
-            local magnitude = (currentPosition - lastPosition).Magnitude
-                
-            -- check to see if defined threshold was exceeded
-            if magnitude > Configuration.MaxMagnitude then
-                -- check to see if player entry is new
-                if lastPosition ~= Vector3.zero then
-                    self:_exceededMaxMagnitude(player)
+    for player: Player, lastPosition: Vector3 in _positionData do
+
+        -- prevent yield for each player
+        task.spawn(function()
+            -- TODO verify player isn't being teleported by the server
+            
+            -- verify a character exists
+            local character = player.Character :: Model?
+            if character then
+                -- verify humanoid exists and player is alive
+                local humanoid = character:FindFirstChild("Humanoid") :: Humanoid?
+                if not humanoid then
+                    return
+                end
+                if humanoid.Health <= 0 then
+                    return
+                end
+
+                -- check magnitude
+                local currentPosition = character:GetPivot().Position
+                local magnitude = (currentPosition - lastPosition).Magnitude
+
+                -- check to see if defined threshold was exceeded
+                if magnitude > _configuration.MaxMagnitude then
+                    -- check to see if player entry is new before firing exceeded
+                    if lastPosition ~= Vector3.zero then
+                        PositionDeltaService:_exceededMaxMagnitude(player)
+                    end
+                end
+
+                -- update entry
+                if player then
+                    _positionData[player] = currentPosition
                 end
             end
-
-            -- update entry
-            if player then
-                self._positionData[player] = currentPosition
-            end
-        end
+        end)
+        
     end
 end
 
-function CheckPositionDeltas:_init()
-    local elapsed = 0
 
-    RunService.Heartbeat:Connect(function(deltaTime: number)
-        -- add time passed
-        elapsed += deltaTime
-        
-        -- determine if it's time for a scan
-        if elapsed >= Configuration.RefreshRate then
-            elapsed = 0
-            self:_scan()
-        end
-    end)
+-- init
+-- Listen for entering / exiting Players
+Players.PlayerAdded:Connect(function(player: Player)
+    PositionDeltaService:_playerAdded(player)
+end)
+Players.PlayerRemoving:Connect(function(player: Player)
+    PositionDeltaService:_playerRemoving(player)
+end)
+for _, player: Player in Players:GetPlayers() do
+    PositionDeltaService:_playerAdded(player)
 end
 
-CheckPositionDeltas.new()
+-- Begin scanning
+RunService.Heartbeat:Connect(function(deltaTime: number)
+    -- add time passed
+    _elapsed += deltaTime
+        
+    -- determine if it's time for a scan
+    if _elapsed >= _configuration.RefreshRate then
+        _elapsed = 0
+        PositionDeltaService:_scan()
+    end
+end)
